726eebed39c9287fb5eb61f2032f1e1e1981ed8f
LeanBow <theleanbow@gmail.com>
2023-06-23 12:13:16 +0545

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <p className='text-red-900 text-center text-4xl hover:text-blue-700 underline'>Shashank Podcast</p>
-       </div>
+      <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+          Shashank Dada ko podcast
+          <br className="max-md:hidden" />
+          <span className="orange_gradient text-center ">Cool Podcasts</span>
+        </h1>
+        <p className="desc text-center">
+          Networking sikne bhai bahini haru ko lagi podcast
+        </p>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
