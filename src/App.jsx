636aede85f7e3d320de93460a36ebf06da15a6ec
LeanBow <theleanbow@gmail.com>
2023-06-23 15:13:57 +0545

    antdesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Navbar from "./Components/Navbar";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
+      <Navbar />
+
       <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
           Shashank Dada ko podcast
